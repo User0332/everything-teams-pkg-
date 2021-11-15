564984aa74cfed688d5664e7af2da80933f35978
User0332 <78310305+User0332@users.noreply.github.com>
2021-11-15 10:51:32 -0500

    Create csharp_utils.py

---
+++ everything/csharp_utils.py
@@ -0,0 +1,19 @@
+def run(filename):
+	try:
+		file = importlib.import_module(filename[0:-3])
+	except ModuleNotFoundError:
+		raise FileNotFoundError(f"File {filename} was not found.")
+	try:
+		exit_code = file.main()
+		if type(exit_code) != int:
+			raise TypeError("Function int main does not return type int")
+		else:
+			exit(exit_code)
+	except AttributeError as e:
+		if str(e) == f"partially initialized module '{filename[0:-3]}' has no attribute 'main' (most likely due to a circular import)":
+			raise MainEntryNotFoundError(filename)
+		else:
+			raise e
+      
+      
+  
